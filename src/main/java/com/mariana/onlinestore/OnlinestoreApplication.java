dc4abbc50f02351cf936c9eefa37df2933267e9f
Mariana Guimarães <mariana.guimr@gmail.com>
2026-05-10 00:25:10 +0100

    Project creation

---
+++ src/main/java/com/mariana/onlinestore/OnlinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.mariana.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinestoreApplication.class, args);
+	}
+
+}
