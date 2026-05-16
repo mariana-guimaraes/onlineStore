9c7e5b215d961187fc755f63f707738481869f04
Mariana Guimarães <mariana.guimr@gmail.com>
2026-05-16 17:45:17 +0100

    Initial configuration for User entity

--- src/test/java/com/mariana/onlinestore/OnlinestoreApplicationTests.java
+++ src/test/java/com/mariana/onlinestore/OnlinestoreApplicationTests.java
@@ -1,13 +1,12 @@
 package com.mariana.onlinestore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class OnlinestoreApplicationTests {
+@SpringBootApplication
+public class OnlinestoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinestoreApplication.class, args);
+    }
 }
