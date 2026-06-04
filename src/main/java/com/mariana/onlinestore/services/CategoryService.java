47bf1480b6a99252453dd7fe323f00b79a30cc52
Mariana Guimarães <mariana.guimr@gmail.com>
2026-06-04 12:02:29 +0100

    Adding Category and Product APIs

--- src/main/java/com/mariana/onlinestore/services/CategoryService.java
+++ src/main/java/com/mariana/onlinestore/services/CategoryService.java
@@ -14,13 +14,27 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
+	}
+
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Category update(Long id, Category obj) {
+		Category entity = repository.getReferenceById(id);
+		entity.setName(obj.getName());
+		return repository.save(entity);
 	}
 }
--- src/main/java/com/mariana/onlinestore/services/ProductService.java
+++ src/main/java/com/mariana/onlinestore/services/ProductService.java
@@ -14,13 +14,30 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	public List<Product> findAll(){
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
+	}
+
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Product update(Long id, Product obj) {
+		Product entity = repository.getReferenceById(id);
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+		return repository.save(entity);
 	}
 }
